e66ea1fc1802127ef1f44dbad34e2558df5f2c0c
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-06-15 06:14:18 +0200

    Switched to my hosted backend 'https://jobbinder.onrender.com'

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  }
+    port: 8000,
+    proxy: {
+      '/api': {
+        target: 'https://jobbinder.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
